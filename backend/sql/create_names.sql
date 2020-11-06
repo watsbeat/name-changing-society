b5dae2a2da28ed866987225db4b81d4786c7b026
watsbeat <c.watsonbeaton@gmail.com>
2020-11-06 11:11:18 +1000

    changing sql

--- backend/sql/create_names.sql
+++ backend/sql/create_names.sql
@@ -1,9 +1,11 @@
 DROP TABLE IF EXISTS names;
 CREATE TABLE names (
     id SERIAL PRIMARY KEY,
+    citizen_id int NOT NULL references citizens(id),
     user_id int NOT NULL references users(id),
-    -- or NUMERIC
-    full_name VARCHAR(255) NOT NULL,
-    start_date DATE NOT NULL,
-    expiry_date DATE NOT NULL
+    first_name VARCHAR(255) NOT NULL,
+    middle_name VARCHAR,
+    last_name VARCHAR NOT NULL
+    held_from DATE NOT NULL,
+    held_to DATE NOT NULL
 );
--- backend/sql/create_users.sql
+++ backend/sql/create_users.sql
@@ -1,7 +1,8 @@
 DROP TABLE IF EXISTS users;
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
+    citizen_id int NOT NULL references citizens(id)
     username VARCHAR(255) UNIQUE NOT NULL,
-    email VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL
+    email VARCHAR(255) UNIQUE NOT NULL,
 );
