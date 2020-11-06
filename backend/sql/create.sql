6a62311b89d2686b00e42028b06b5e9072db40f6
watsbeat <c.watsonbeaton@gmail.com>
2020-11-06 12:28:12 +1000

    update create script

--- backend/sql/create.sql
+++ backend/sql/create.sql
@@ -1,10 +1,25 @@
 BEGIN;
+
+-- Create session table per https://github.com/voxpelli/node-connect-pg-simple
+DROP TABLE IF EXISTS session;
+CREATE TABLE "session" (
+  "sid" varchar NOT NULL COLLATE "default",
+	"sess" json NOT NULL,
+	"expire" timestamp(6) NOT NULL
+)
+WITH (OIDS=FALSE);
+ALTER TABLE "session" ADD CONSTRAINT "session_pkey" PRIMARY KEY ("sid") NOT DEFERRABLE INITIALLY IMMEDIATE;
+CREATE INDEX "IDX_session_expire" ON "session" ("expire");
+
+-- Create citizens table
 DROP TABLE IF EXISTS citizens CASCADE;
 CREATE TABLE citizens (
     id SERIAL PRIMARY KEY,
     user_id int NOT NULL references users(id)
 );
 DROP TABLE IF EXISTS users CASCADE;
+
+-- Create users table
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     citizen_id int NOT NULL references citizens(id),
@@ -12,6 +27,8 @@ CREATE TABLE users (
     password VARCHAR(255) NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL
 );
+
+-- Create names table
 DROP TABLE IF EXISTS names CASCADE;
 CREATE TABLE names (
     id SERIAL PRIMARY KEY,
@@ -23,4 +40,5 @@ CREATE TABLE names (
     held_from DATE NOT NULL,
     held_to DATE NOT NULL
 );
+
 COMMIT;
