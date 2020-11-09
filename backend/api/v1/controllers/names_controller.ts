c6c70eefc8825efa938a2528135583be31ec3ae1
watsbeat <c.watsonbeaton@gmail.com>
2020-11-09 11:24:52 +1000

    fix listNamesExpiring function

--- backend/api/v1/controllers/names_controller.ts
+++ backend/api/v1/controllers/names_controller.ts
@@ -6,7 +6,7 @@ export default new (class Names {
         try {
             // TODO: It's possibly better to select held_to as is and use moment-timezone to format it later
             const oneYearOld = moment().tz('Australia/Brisbane').subtract(1, 'years').format('YYYY-MM-DD');
-            const twentyEightDaysFromExpiring = moment.tz('Australia/Brisbane').subtract(1, 'years').subtract(28, 'days').format('YYYY-MM-DD');
+            const twentyEightDaysFromExpiring = moment.tz('Australia/Brisbane').subtract(1, 'years').add(28, 'days').format('YYYY-MM-DD');
             return await db.any(
                 `SELECT
                     first_name,
@@ -15,7 +15,7 @@ export default new (class Names {
                     TO_CHAR((held_from + interval '1 year'), 'yyyy-mm-dd') AS held_to
                 FROM names
                 WHERE held_from BETWEEN $1::date AND $2::date`,
-                [twentyEightDaysFromExpiring, oneYearOld]
+                [oneYearOld, twentyEightDaysFromExpiring]
             );
         } catch (err) {
             console.error(err);
